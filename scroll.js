ecac424ef3986e53f5babd0731e5eddb96d7e60a
Subhaditya <msubhaditya@gmail.com>
2019-12-16 15:06:47 +0530

    fixed scroll

--- scroll.js
+++ scroll.js
@@ -1,17 +1,24 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.scroll-top').fadeIn();
-    } else {
-      $('.scroll-top').fadeOut();
+$(document).ready(function ()
+{
+    $("a").on('click', function (event)
+    {
+
+        if (this.hash !== "")
+        {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            }, 100, function ()
+            {
+
+                window.location.hash = hash;
+            }
+            );
+        }
     }
-  });
-
-  $('.scroll-top').click(function () {
-    $("html, body").animate({
-      scrollTop: 0
-    }, 100);
-      return false;
-  });
-
-});
+    );
+}
+);
