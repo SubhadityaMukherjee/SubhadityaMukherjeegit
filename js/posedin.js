5d9afd5413944d0c0e5851eb0b6f9709750164cd
SubhadityaMukherjee <msubhaditya@gmail.com>
2020-04-23 01:30:52 +0400

    added dinofix1

--- js/posedin.js
+++ js/posedin.js
@@ -27,7 +27,7 @@ function setup()
             let l_elbow = temp.pose.leftElbow.x;
             let r_conf = temp.pose.rightWrist.confidence;
             let l_conf = temp.pose.leftWrist.confidence;
-            
+            document.getElementById("test").innerHTML = ("no",r_conf, r_wrist);
             console.log("right- ",r_wrist,r_conf);
             if ((r_wrist >0)&&(r_conf>=.1))
             {
