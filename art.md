2722d1f3afc025433cf302cff5b66c5eb38bce08
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-03-06 23:25:58 +0400

    finally_art

--- art.md
+++ art.md
@@ -5,5 +5,5 @@ description: "Here is my work"
 
 photos:
     set : berlin
-    size: 40
+    size: 20
 ---
--- artworks/renamer.py
+++ artworks/renamer.py
@@ -3,11 +3,10 @@ import os
 _src = "/media/hdd/github/SubhadityaMukherjeegit/artworks/"
 _ext = ".png"
 
-endsWithNumber = re.compile(r'(\d+)'+(re.escape(_ext))+'$')
-lookin = [_src+x for x in os.listdir(_src) if "png" in x]
-for filename in lookin:
-    m = endsWithNumber.search(filename)
-    if m:
-        os.rename(filename, _src+'rename-' + str(m.group(1)).zfill(3)+_ext)
-    else:
-        os.rename(filename, _src+'rename-' + str(0).zfill(3)+_ext)
+lookin = [x for x in os.listdir(_src) if "png" in x]
+no = 1
+for num,filename in enumerate(lookin):
+    os.rename(_src+filename , f"{_src}rename-{str(no)}.png")
+    no += 1
+    
+    
