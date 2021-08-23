4b926b3c49651406f67b6e09ced8436433546c45
SubhadityaMukherjee <msubhaditya@gmail.com>
2021-08-23 13:24:18 +0200

    up

--- pusher.sh
+++ pusher.sh
@@ -1,4 +1,3 @@
-python /media/hdd/github/selket/runner.py -n "SubhadityaMukherjeegit"
-if [[ ! -z $1 ]]; then
-        git add . && git commit -m $1 && git push
-fi
+python3 /media/hdd/github/selket/runner.py -n "SubhadityaMukherjeegit"
+git add . && git commit -m $1 && git push
+
